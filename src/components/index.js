e6855a8d8ee5842586ad39e45de11b6f0fc89953
BuildTools <lily@l1lith.com>
2022-04-01 02:51:59 -0600

    setup the build environment

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Entity } from './Entity'
+export { default as Melon } from './Melon'
+export { default as Preloader } from './Preloader'
+export { default as Stage } from './Stage'
